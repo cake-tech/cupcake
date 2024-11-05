0ffb1fac60947bd7ce80b764eb3a05502174e550
OmarHatem <omarh.ismail1@gmail.com>
2024-11-05 07:40:21 +0200

    minor things for release

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Cup Cake',
       themeMode: ThemeMode.dark,
       darkTheme: darkBaseTheme,
