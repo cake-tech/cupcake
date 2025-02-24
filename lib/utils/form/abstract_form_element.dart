e62b933e25b9da186967a0d23794b2468e57ed2c
cyan <cyjan@mrcyjanek.net>
2025-02-24 06:22:13 +0100

    Update lib/utils/form/abstract_form_element.dart
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>

--- lib/utils/form/abstract_form_element.dart
+++ lib/utils/form/abstract_form_element.dart
@@ -1,5 +1,5 @@
 abstract class FormElement {
   bool get isOk => true;
-  String get label => throw UnimplementedError();
+  String get label => "";
   Future<String> get value => throw UnimplementedError();
 }
