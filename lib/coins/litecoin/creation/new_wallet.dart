a2b1ae9e48e1e65986857df17a4eee2d4b92791f
cyan <cyjan@mrcyjanek.net>
2025-11-10 00:56:28 +0100

    Update lib/coins/litecoin/creation/new_wallet.dart
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>

--- lib/coins/litecoin/creation/new_wallet.dart
+++ lib/coins/litecoin/creation/new_wallet.dart
@@ -35,7 +35,7 @@ class CreateLitecoinWalletCreationMethod extends CreationMethod {
 
     final keys = "${Litecoin().getPathForWallet(p.basename(walletPath))}.keys";
     final keysEncrypted =
-        DefaultEncryption().encryptString("$mnemonic:$passphrase", walletPassword);
+        DefaultEncryption().encryptString("$mnemonic;$passphrase", walletPassword);
     File(keys).writeAsBytesSync(keysEncrypted);
 
     return CreationOutcome(
